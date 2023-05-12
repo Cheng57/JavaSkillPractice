fe7b49a9ec4477a767386d0ad4f4ace8dc8138f3
Cheng Wu <cwu57cn@gmail.com>
2023-05-11 23:14:16 -0500

    Add a counter and two last lines
    The program now can do any bottle(s) of beer

--- NinetynineBottlesBeer/NinetynineBottlesBeer.java
+++ NinetynineBottlesBeer/NinetynineBottlesBeer.java
@@ -1,6 +1,7 @@
 public class NinetynineBottlesBeer{
 	public static void main(String[] args){
-		int num = 99;
+		int num = 1;
+		int count = 0;
 		String word = "bottles";
 
 		while (num > 0){
@@ -13,6 +14,7 @@ public class NinetynineBottlesBeer{
 			System.out.print("Take one down and pass it around, ");
 
 			num = num - 1;
+			count = count + 1;
 
 			if (num > 1){
 				System.out.println(num + " " + word + " of beer on the wall.");
@@ -23,8 +25,19 @@ public class NinetynineBottlesBeer{
 				System.out.println(num + " " + word + " of beer on the wall.");
 				System.out.println();
 			}
-			else{
-				System.out.println("No more bottles of beer on the wall.");
+			else {
+				System.out.println("no more bottles of beer on the wall.");
+				System.out.println();
+				System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+
+				if (count == 1){
+					System.out.println("Go to the store and buy some more, " + count + " " +
+									word + " of beer on the wall.");
+				}
+				else {
+					System.out.println("Go to the store and buy some more, " + count + " " +
+									word + "s of beer on the wall.");
+				}
 			}
 		}
 	}
