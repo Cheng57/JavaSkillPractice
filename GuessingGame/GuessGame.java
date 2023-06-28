02ffd089ed4d8fb3b0b5637a4419fe110fa4a057
Cheng Wu <cwu57cn@gmail.com>
2023-06-28 16:40:55 -0500

    Update GuessGame

--- GuessingGame/GuessGame.java
+++ GuessingGame/GuessGame.java
@@ -3,7 +3,7 @@ public class GuessGame{
 	Player p2;
 	Player p3;
 
-	Public void startGame{
+	public void startGame(){
 		p1 = new Player();
 		p2 = new Player();
 		p3 = new Player();
@@ -16,7 +16,7 @@ public class GuessGame{
 		boolean p2isRight = false;
 		boolean p3isRight = false;
 
-		int targetNumber = (int) (Math.random * 10);
+		int targetNumber = (int) (Math.random() * 10);
 		System.out.println("I'm thinking of a number between 0 and 9...");
 
 		while(true){
