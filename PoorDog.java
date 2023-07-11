4db36251ddd471f00e83fc87af06c945685f1fdc
Cheng Wu <cwu57cn@gmail.com>
2023-07-11 14:40:47 -0500

    Create PoorDog file

---
+++ PoorDog.java
@@ -0,0 +1,14 @@
+class PoorDog{
+	private int size; // instance variable
+	private String name; // instance variable
+
+	// method
+	public int getSize(){
+		return size;
+	}
+
+	// method
+	public String getName(){
+		return name;
+	}
+}
