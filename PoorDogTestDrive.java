6095315bb7fc693085db62f495676fda82e9fc70
Cheng Wu <cwu57cn@gmail.com>
2023-07-11 14:42:07 -0500

    Create PoorDog Test file

---
+++ PoorDogTestDrive.java
@@ -0,0 +1,9 @@
+class PoorDogTestDrive{
+	public static void main(String[] args){
+		PoorDog d1 = new PoorDog(); // declare and create an object of PoorDog
+
+		// print out instance variables
+		System.out.println("Dog size is " + d1.getSize());
+		System.out.println("Dog name is "+ d1.getName());
+	}
+}
